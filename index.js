63cfa94648b174ef8923041681df990e9b4bfce6
Samara Flor <samaraflor2013>
2023-05-25 09:09:37 -0300

    adicionando jwt para autenticação

--- index.js
+++ index.js
@@ -2,23 +2,53 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+
+const JWTSecret = "djkshahjksdajksdhasjkdhasjkdhasjkdhasjkd";
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+function auth(req, res, next){
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+
+        jwt.verify(token,JWTSecret,(err, data) => {
+            if(err){
+                res.status(401);
+                res.json({err:"Token inválido!"});
+            }else{
+
+                req.token = token;
+                req.loggedUser = {id: data.id,email: data.email};
+                req.empresa = "Guia do programador";                
+                next();
+            }
+        });
+    }else{
+        res.status(401);
+        res.json({err:"Token inválido!"});
+    } 
+}
+
 var DB = {
     games: [
         {
             id: 23,
-            title: "Super Mario",
+            title: "Call of duty MW",
             year: 2019,
             price: 60
         },
         {
             id: 65,
-            title: "Free fire",
+            title: "Sea of thieves",
             year: 2018,
             price: 40
         },
@@ -28,15 +58,29 @@ var DB = {
             year: 2012,
             price: 20
         }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Samara flor",
+            email: "samara@email.com.br",
+            password: "1234"
+        },
+        {
+            id: 20,
+            name: "Maria",
+            email: "maria@email.com",
+            password: "4567"
+        }
     ]
 }
 
-app.get("/games",(req, res) => {
+app.get("/games",auth,(req, res) => {
     res.statusCode = 200;
-    res.json(DB.games);
+    res.json({empresa: req.empresa,user: req.loggedUser,games: DB.games});
 });
 
-app.get("/game/:id",(req, res) => {
+app.get("/game/:id",auth,(req, res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
@@ -54,7 +98,7 @@ app.get("/game/:id",(req, res) => {
     }
 });
 
-app.post("/game",(req, res) => { 
+app.post("/game",auth,(req, res) => { 
     var {title, price, year} = req.body;
     DB.games.push({
         id: 2323,
@@ -65,7 +109,7 @@ app.post("/game",(req, res) => {
     res.sendStatus(200);
 })
 
-app.delete("/game/:id",(req, res) => {
+app.delete("/game/:id",auth,(req, res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
@@ -117,6 +161,40 @@ app.put("/game/:id",(req, res) => {
 
 });
 
-app.listen(45678,() => {
+
+app.post("/auth",(req, res) => {
+
+    var {email, password} = req.body;
+
+    if(email != undefined){
+
+        var user = DB.users.find(u => u.email == email);
+        if(user != undefined){
+            if(user.password == password){
+                jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn:'48h'},(err, token) => {
+                    if(err){
+                        res.status(400);
+                        res.json({err:"Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                })
+            }else{
+                res.status(401);
+                res.json({err: "Credenciais inválidas!"});
+            }
+        }else{
+            res.status(404);
+            res.json({err: "O E-mail enviado não existe na base de dados!"});
+        }
+
+    }else{
+        res.status(400);
+        res.send({err: "O E-mail enviado é inválido"});
+    }
+});
+
+app.listen(45679,() => {
     console.log("API RODANDO!");
 });
